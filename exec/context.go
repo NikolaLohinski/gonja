d839c1ed8c3de810975bf929db99c613a1a2a910
NikolaLohinski <nikolalohinski@gmail.com>
2024-02-27 18:05:20 +0100

    fix(exec): avoid concurrency issues with context

--- exec/context.go
+++ exec/context.go
@@ -1,8 +1,11 @@
 package exec
 
+import "sync"
+
 type Context struct {
 	data   map[string]interface{}
 	parent *Context
+	lock   sync.Mutex
 }
 
 func NewContext(data map[string]interface{}) *Context {
@@ -14,7 +17,9 @@ func EmptyContext() *Context {
 }
 
 func (ctx *Context) Has(name string) bool {
+	ctx.lock.Lock()
 	_, exists := ctx.data[name]
+	ctx.lock.Unlock()
 	if !exists && ctx.parent != nil {
 		return ctx.parent.Has(name)
 	}
@@ -22,7 +27,9 @@ func (ctx *Context) Has(name string) bool {
 }
 
 func (ctx *Context) Get(name string) (interface{}, bool) {
+	ctx.lock.Lock()
 	value, exists := ctx.data[name]
+	ctx.lock.Unlock()
 	if exists {
 		return value, true
 	} else if ctx.parent != nil {
@@ -33,23 +40,30 @@ func (ctx *Context) Get(name string) (interface{}, bool) {
 }
 
 func (ctx *Context) Set(name string, value interface{}) {
+	ctx.lock.Lock()
 	ctx.data[name] = value
+	ctx.lock.Unlock()
 }
 
 func (ctx *Context) Inherit() *Context {
-	return &Context{
+	ctx.lock.Lock()
+	inherited := &Context{
 		data:   map[string]interface{}{},
 		parent: ctx,
 	}
+	ctx.lock.Unlock()
+	return inherited
 }
 
 // Update updates this context with the key/value pairs from a map.
 func (ctx *Context) Update(other *Context) *Context {
+	ctx.lock.Lock()
 	if other == nil {
 		return ctx
 	}
 	for k, v := range other.data {
 		ctx.data[k] = v
 	}
+	ctx.lock.Unlock()
 	return ctx
 }
