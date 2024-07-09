28d6151f7ec718c1343e9474e71152d5ce46a8e0
Mikael Rapp <micke.rapp@gmail.com>
2024-07-10 00:47:25 +0200

    chore: fix todo make getNestedKwArgs more robust - handle just about any data structure

--- builtins/methods/pystring/kwargs.go
+++ builtins/methods/pystring/kwargs.go
@@ -2,6 +2,7 @@ package pystring
 
 import (
 	"fmt"
+	"reflect"
 )
 
 // Used to resolve nested kw-args
@@ -41,17 +42,134 @@ func getNestedKwArgs(keys []string, kwarg AttributeGetter) (any, error) {
 	}
 
 	// See if we can recurse down to sub-keys
-	if subKwarg, ok := maybeVal.(AttributeGetter); ok {
-		return getNestedKwArgs(tail, subKwarg)
+	switch val := maybeVal.(type) {
+	case AttributeGetter:
+		return getNestedKwArgs(tail, val)
+	case map[string]any:
+		return getNestedKwArgs(tail, KwArgs(val))
+	case map[string]string:
+		subKwarg := make(map[string]any)
+		for k, v := range val {
+			subKwarg[k] = v
+		}
+		return getNestedKwArgs(tail, KwArgs(subKwarg))
+	case map[string]AttributeGetter:
+		subKwarg := make(map[string]any)
+		for k, v := range val {
+			subKwarg[k] = v
+		}
+		return getNestedKwArgs(tail, KwArgs(subKwarg))
+	case map[string]bool:
+		subKwarg := make(map[string]any)
+		for k, v := range val {
+			subKwarg[k] = v
+		}
+		return getNestedKwArgs(tail, KwArgs(subKwarg))
+	case map[string]int:
+		subKwarg := make(map[string]any)
+		for k, v := range val {
+			subKwarg[k] = v
+		}
+		return getNestedKwArgs(tail, KwArgs(subKwarg))
+	case map[string]int8:
+		subKwarg := make(map[string]any)
+		for k, v := range val {
+			subKwarg[k] = v
+		}
+		return getNestedKwArgs(tail, KwArgs(subKwarg))
+	case map[string]int16:
+		subKwarg := make(map[string]any)
+		for k, v := range val {
+			subKwarg[k] = v
+		}
+		return getNestedKwArgs(tail, KwArgs(subKwarg))
+	case map[string]int32:
+		subKwarg := make(map[string]any)
+		for k, v := range val {
+			subKwarg[k] = v
+		}
+		return getNestedKwArgs(tail, KwArgs(subKwarg))
+	case map[string]int64:
+		subKwarg := make(map[string]any)
+		for k, v := range val {
+			subKwarg[k] = v
+		}
+		return getNestedKwArgs(tail, KwArgs(subKwarg))
+	case map[string]uint:
+		subKwarg := make(map[string]any)
+		for k, v := range val {
+			subKwarg[k] = v
+		}
+		return getNestedKwArgs(tail, KwArgs(subKwarg))
+	case map[string]uint8:
+		subKwarg := make(map[string]any)
+		for k, v := range val {
+			subKwarg[k] = v
+		}
+		return getNestedKwArgs(tail, KwArgs(subKwarg))
+	case map[string]uint16:
+		subKwarg := make(map[string]any)
+		for k, v := range val {
+			subKwarg[k] = v
+		}
+		return getNestedKwArgs(tail, KwArgs(subKwarg))
+	case map[string]uint32:
+		subKwarg := make(map[string]any)
+		for k, v := range val {
+			subKwarg[k] = v
+		}
+		return getNestedKwArgs(tail, KwArgs(subKwarg))
+	case map[string]uint64:
+		subKwarg := make(map[string]any)
+		for k, v := range val {
+			subKwarg[k] = v
+		}
+		return getNestedKwArgs(tail, KwArgs(subKwarg))
+	case map[string]float32:
+		subKwarg := make(map[string]any)
+		for k, v := range val {
+			subKwarg[k] = v
+		}
+		return getNestedKwArgs(tail, KwArgs(subKwarg))
+	case map[string]float64:
+		subKwarg := make(map[string]any)
+		for k, v := range val {
+			subKwarg[k] = v
+		}
+		return getNestedKwArgs(tail, KwArgs(subKwarg))
+
+	default:
+		// Handle structs
+		v := reflect.ValueOf(maybeVal)
+		if v.Kind() == reflect.Ptr {
+			v = v.Elem()
+		}
+
+		if v.Kind() == reflect.Struct {
+			// Check if the struct implements AttributeGetter
+			if getter, ok := maybeVal.(AttributeGetter); ok {
+				return getNestedKwArgs(tail, getter)
+			}
+
+			// Otherwise, use reflection to access the fields
+			fieldMap := make(map[string]any)
+			for i := 0; i < v.NumField(); i++ {
+				field := v.Type().Field(i)
+				fieldValue := v.Field(i).Interface()
+				fieldMap[field.Name] = fieldValue
+			}
+			return getNestedKwArgs(tail, KwArgs(fieldMap))
+		}
+
+		// Handle maps of structs
+		if v.Kind() == reflect.Map && v.Type().Key().Kind() == reflect.String {
+			mapField := make(map[string]any)
+			for _, key := range v.MapKeys() {
+				mapField[key.String()] = v.MapIndex(key).Interface()
+			}
+			return getNestedKwArgs(tail, KwArgs(mapField))
+		}
 	}
-	if subMap, ok := maybeVal.(map[string]any); ok {
-		return getNestedKwArgs(tail, KwArgs(subMap))
-	}
-	// TODO: support more type assertions such as map[string]string,
-	// TODO: support more type assertions such as map[string]AttributeGetter,
-	// if len(tail) == 0  then we can also check basic types?? no... prob not... might need reflection
-	// TODO: support more type assertions such as map[string]bool,
-	// TODO: support index string
 
 	return "", fmt.Errorf("%w: '%#v' is not a sub-gettable for key %s", ErrValue, maybeVal, key)
 }
---
+++ builtins/methods/pystring/kwargs_test.go
@@ -0,0 +1,121 @@
+package pystring
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("GetNestedKwArgs", func() {
+	type Nested struct {
+		Field string
+	}
+
+	type Root struct {
+		Nested Nested
+	}
+
+	DescribeTable("retrieves the correct value",
+		func(keys []string, kwarg AttributeGetter, expected any, expectError bool) {
+			result, err := getNestedKwArgs(keys, kwarg)
+			if expectError {
+				Expect(err).To(HaveOccurred())
+			} else {
+				Expect(err).NotTo(HaveOccurred())
+				Expect(result).To(Equal(expected))
+			}
+		},
+		Entry("simple key",
+			[]string{"root", "Nested", "Field"},
+			KwArgs{
+				"root": Root{
+					Nested: Nested{
+						Field: "value",
+					},
+				},
+			},
+			"value",
+			false,
+		),
+		Entry("missing key",
+			[]string{"root", "Missing"},
+			KwArgs{
+				"root": Root{
+					Nested: Nested{
+						Field: "value",
+					},
+				},
+			},
+			"",
+			true,
+		),
+		Entry("map of any",
+			[]string{"map", "key"},
+			KwArgs{
+				"map": map[string]any{
+					"key": "value",
+				},
+			},
+			"value",
+			false,
+		),
+		Entry("map of string",
+			[]string{"map", "key"},
+			KwArgs{
+				"map": map[string]string{
+					"key": "value",
+				},
+			},
+			"value",
+			false,
+		),
+		Entry("map of int",
+			[]string{"map", "key"},
+			KwArgs{
+				"map": map[string]int{
+					"key": 42,
+				},
+			},
+			42,
+			false,
+		),
+		Entry("struct without AttributeGetter",
+			[]string{"root", "Nested", "Field"},
+			KwArgs{
+				"root": struct {
+					Nested struct {
+						Field string
+					}
+				}{
+					Nested: struct {
+						Field string
+					}{
+						Field: "value",
+					},
+				},
+			},
+			"value",
+			false,
+		),
+		Entry("struct with AttributeGetter",
+			[]string{"root", "Field"},
+			KwArgs{
+				"root": testStructWithGetter{
+					Field: "value",
+				},
+			},
+			"value",
+			false,
+		),
+	)
+})
+
+type testStructWithGetter struct {
+	Field string
+}
+
+func (t testStructWithGetter) Get(key string) (any, bool) {
+	if key == "Field" {
+		return t.Field, true
+	}
+	return nil, false
+}
