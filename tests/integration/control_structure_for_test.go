fb31f317558af24fface68fa0ad0a2959bc3e8de
Burak Karakan <burak.karakan@gmail.com>
2026-03-06 18:56:14 -0800

    test: cover loop.length in for loops

---
+++ tests/integration/control_structure_for_test.go
@@ -0,0 +1,58 @@
+package integration_test
+
+import (
+	"github.com/nikolalohinski/gonja/v2"
+	"github.com/nikolalohinski/gonja/v2/exec"
+	"github.com/nikolalohinski/gonja/v2/loaders"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Context("control structure 'for'", func() {
+	var (
+		identifier = new(string)
+
+		environment = new(*exec.Environment)
+		loader      = new(loaders.Loader)
+
+		context = new(*exec.Context)
+
+		returnedResult = new(string)
+		returnedErr    = new(error)
+		shouldRender   = func(template, result string) {
+			Context(template, func() {
+				BeforeEach(func() {
+					*loader = loaders.MustNewMemoryLoader(map[string]string{
+						*identifier: template,
+					})
+				})
+				It("should return the expected rendered content", func() {
+					By("not returning any error")
+					Expect(*returnedErr).To(BeNil())
+					By("returning the expected result")
+					AssertPrettyDiff(result, *returnedResult)
+				})
+			})
+		}
+	)
+	BeforeEach(func() {
+		*identifier = "/test"
+		*environment = gonja.DefaultEnvironment
+		*loader = loaders.MustNewMemoryLoader(nil)
+	})
+	JustBeforeEach(func() {
+		var t *exec.Template
+		t, *returnedErr = exec.NewTemplate(*identifier, gonja.DefaultConfig, *loader, *environment)
+		if *returnedErr != nil {
+			return
+		}
+		*returnedResult, *returnedErr = t.ExecuteToString(*context)
+	})
+	Context("loop metadata", func() {
+		shouldRender(
+			"{% for item in [1, 2, 3, 4] if item > 2 %}{{ loop.length }}:{{ loop.index }}:{{ item }}{% if not loop.last %}|{% endif %}{% endfor %}",
+			"2:1:3|2:2:4",
+		)
+	})
+})
