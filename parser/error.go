71ac06dfa32296a339cdc9c5b32f5d01ad2764fb
pi66 <pixel2176@proton.me>
2026-09-22 22:56:30 +0100

    parser: add structured SyntaxError type

--- parser/error.go
+++ parser/error.go
@@ -1,14 +1,36 @@
 package parser
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/nikolalohinski/gonja/v2/tokens"
-	"github.com/pkg/errors"
 )
+
+type SyntaxError struct {
+	Message string
+	Raw      string
+	Line     int
+	Column   int
+}
+
+func (e *SyntaxError) Error() string {
+	if e.Raw == "" && e.Line == 0 && e.Column == 0 {
+		return e.Message
+	}
+
+	return fmt.Sprintf(`%s (Line: %d Col: %d, near "%s")`, e.Message, e.Line, e.Column, e.Raw)
+}
 
 func (p *Parser) Error(message string, token *tokens.Token) error {
 	if token == nil {
 		return errors.New(message)
 	}
 
-	return errors.Errorf(`%s (Line: %d Col: %d, near "%s")`, message, token.Line, token.Col, token.Val)
+	return &SyntaxError{
+		Message: message,
+		Raw:     token.Val,
+		Line:    token.Line,
+		Column:  token.Col,
+	}
 }
